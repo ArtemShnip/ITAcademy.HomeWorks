631c846c70c4f2ac003509c2c3473ff3f5bc8087
Artem Shnip <tjoma27@gmail.com>
2019-12-15 14:48:29 +0300

    done

---
+++ hw01/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW04._1_Unicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Bletter;
+            char Sletter;
+            for (int i = 90,j=122 ; i > 64; i--, j--)       
+            {
+                Bletter = Convert.ToChar(i);
+                Sletter = Convert.ToChar(j);
+                Console.WriteLine(Bletter + " " + Sletter);     
+            }
+        }
+    }
+}
