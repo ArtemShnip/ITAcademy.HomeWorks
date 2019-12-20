9eb91c4b65af0f0d4f6ad52e02d3caf6f0f25e1e
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2019-12-20 15:34:24 +0300

    разница в 6.39с

---
+++ hw01/Task4_1/Task4_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task4_1
+{
+    class Task4_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press to start");
+            Console.ReadLine();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Int64[] array = new Int64[100000000];
+            for (Int64 i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next();
+            }
+            for (Int64 first = 0, last = array.Length - 1; first < last; first++, last--)
+            {
+                Int64 elementFirst = array[first];
+                Int64 elementLast = array[last];
+                array[first] = elementLast;
+                array[last] = elementFirst;
+            }
+            stopwatch.Stop();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            Console.WriteLine(timeSpan);
+
+        }
+    }
+}
---
+++ hw01/Task4_2/Task4_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task4_2
+{
+    class Task4_2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press to start");
+            Console.ReadLine();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Int64[] array = new Int64[100000000];
+            for (Int64 i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next();
+            }
+            
+            Array.Reverse(array);
+
+            stopwatch.Stop();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            Console.WriteLine(timeSpan);
+
+        }
+    }
+}
