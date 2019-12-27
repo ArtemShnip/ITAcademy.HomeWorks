fcc8bce8652e8be9efc5344657ed0b9a13a7a4dc
Artem Shnip <tjoma27@gmail.com>
2019-12-28 02:54:55 +0300

    task2.3 done

--- hw01/Task_2/Task2.cs
+++ hw01/Task_2/Task2.cs
@@ -6,7 +6,7 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            string poem = "Two roads diverged in a yellow wood And sorry I could not travel both";
+            string poem = "Two roads diverged in a yellow wood, And sorry I could not travel both";
             Console.WriteLine("main:\n" + poem);
             string[] words = poem.Split(' ');
             int maxLength = 0;
@@ -27,26 +27,55 @@ namespace Task_2
                 }
             }
             Item1(maxIndex, words);
-            Item2(maxIndex, maxLength, minIndex, minLength, words);
+            Item2(maxIndex, minIndex, words);
+            Item3(words);
+            Item4();
             Console.ReadLine();
         }
         static void Item1(int maxIndex,string[] words)
         {
-            string[] wordsItem1 = (string[])words.Clone();
             Console.WriteLine("\ntask1:");
-           Array.Clear(wordsItem1, maxIndex, 1);
+            string[] wordsItem1 = (string[])words.Clone();
+            Array.Clear(wordsItem1, maxIndex, 1);
             String output = String.Join(" ", wordsItem1);
             Console.WriteLine(output);
         }
-        static void Item2(int maxIndex,int maxLength,int minIndex,int minLength, string[] words)
+        static void Item2(int maxIndex,int minIndex, string[] words)
         {
-            string[] wordsItem2 = words;
             Console.WriteLine("\ntask2:");
+            string[] wordsItem2 = (string[])words.Clone();
             string temporary = wordsItem2[minIndex];
             wordsItem2[minIndex] = wordsItem2[maxIndex];
             wordsItem2[maxIndex] = temporary;
             String output = String.Join(" ", wordsItem2);
             Console.WriteLine(output);
+        }
+        static void Item3(string[] words)
+        {
+            Console.WriteLine("\ntask3:");
+            string[] wordsItem3 = (string[])words.Clone();
+            char[] signs = new char[10] { '.', ',', ';', ';', '?', '!', '-', '(', ')', '\"' };
+            int countSigns = 0;
+            int countLeters = 0;
+            for (int i = 0; i < wordsItem3.Length; i++)
+            {
+                for (int j = 0; j < wordsItem3[i].Length; j++)
+                {
+                    if (wordsItem3[i].Contains(signs[j]))
+                    {
+                        countSigns++;
+                    }
+                    else
+                    {
+                        countLeters++;
+                    }
+                }
+            }
+            Console.WriteLine("leters: " + countLeters + " \nsigns: " + countSigns);
+        }
+        static void Item4()
+        {
+
         }
     }
 }
