b9aa815d0247c2435788b668bf2291d8984453a5
Artem Shnip <tjoma27@gmail.com>
2019-12-28 01:49:04 +0300

    task2.2 done

--- hw01/Task_2/Task2.cs
+++ hw01/Task_2/Task2.cs
@@ -8,30 +8,45 @@ namespace Task_2
         {
             string poem = "Two roads diverged in a yellow wood And sorry I could not travel both";
             Console.WriteLine("main:\n" + poem);
-            Item1(poem);
-            Item2(poem);
+            string[] words = poem.Split(' ');
+            int maxLength = 0;
+            int maxIndex = 0;
+            int minLength = 1000;
+            int minIndex = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > maxLength)
+                {
+                    maxLength = words[i].Length;
+                    maxIndex = i;
+                }
+                if (words[i].Length < minLength)
+                {
+                    minLength = words[i].Length;
+                    minIndex = i;
+                }
+            }
+            Item1(maxIndex, words);
+            Item2(maxIndex, maxLength, minIndex, minLength, words);
             Console.ReadLine();
         }
-        static void Item1(string poem)
+        static void Item1(int maxIndex,string[] words)
         {
-            Console.WriteLine("\ntask1:\n"+ poem);
-            Console.WriteLine();
-            string[] words = poem.Split(' ');
-            int max = 0;
-            for (int i = 0; i < words.Length; i++)
-            {
-                if (words[i].Length > max) max = words[i].Length;
-            }
-            for (int i = 0; i < words.Length; i++)
-            {
-                if (words[i].Length == max) Array.Clear(words, i, 1);
-            }
-            String output = String.Join(" ", words);
+            string[] wordsItem1 = (string[])words.Clone();
+            Console.WriteLine("\ntask1:");
+           Array.Clear(wordsItem1, maxIndex, 1);
+            String output = String.Join(" ", wordsItem1);
             Console.WriteLine(output);
         }
-        static void Item2(string poem)
+        static void Item2(int maxIndex,int maxLength,int minIndex,int minLength, string[] words)
         {
-
+            string[] wordsItem2 = words;
+            Console.WriteLine("\ntask2:");
+            string temporary = wordsItem2[minIndex];
+            wordsItem2[minIndex] = wordsItem2[maxIndex];
+            wordsItem2[maxIndex] = temporary;
+            String output = String.Join(" ", wordsItem2);
+            Console.WriteLine(output);
         }
     }
 }
