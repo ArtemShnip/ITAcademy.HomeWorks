859a2cc35dbe1a0b8b1a41b9f0758767c8d9db36
Artem Shnip <tjoma27@gmail.com>
2019-12-27 23:29:07 +0300

    task 1 and task 2.1

---
+++ hw01/Task_1/Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a poem in one line, separating lines \';\': ");
+            string poem = " Two roads diverged in a yellow wood,; And sorry I could not travel both; And be one traveler, long I stood; And looked down one as far as I could; To where it bent in the undergrowth.";
+            Console.WriteLine("\n\noutput:\n" + poem);
+            string[] poemLines = poem.Split(new char[] {';'});
+            Console.WriteLine("\n\n");
+            for (int i = 0; i < poemLines.Length; i++)
+            {
+                poemLines[i] = poemLines[i].ToUpper().Replace('O', 'A');
+                Console.WriteLine(poemLines[i]);
+            }
+        }
+    }
+}
