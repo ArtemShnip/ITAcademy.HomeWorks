4371b22220fd9f7b9d18a4b073a69d7458895e24
Artem Shnip <tjoma27@gmail.com>
2019-12-27 23:29:07 +0300

    task 1 and task 2.1

---
+++ hw01/Task_2/Task2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            string poem = "Two roads diverged in a yellow wood And sorry I could not travel both";
+            Console.WriteLine("main:\n" + poem);
+            Item1(poem);
+            Item2(poem);
+            Console.ReadLine();
+        }
+        static void Item1(string poem)
+        {
+            Console.WriteLine("\ntask1:\n"+ poem);
+            Console.WriteLine();
+            string[] words = poem.Split(' ');
+            int max = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > max) max = words[i].Length;
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length == max) Array.Clear(words, i, 1);
+            }
+            String output = String.Join(" ", words);
+            Console.WriteLine(output);
+        }
+        static void Item2(string poem)
+        {
+
+        }
+    }
+}
