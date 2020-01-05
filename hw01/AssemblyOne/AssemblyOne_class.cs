bd0d4bd1e1fe06dd6f8639e348a8b082e7b88393
Artem Shnip <tjoma27@gmail.com>
2020-01-05 20:36:54 +0300

    Task 2 done

---
+++ hw01/AssemblyOne/AssemblyOne_class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+    public class AssemblyOne_class
+    {
+        static void Main(string[] args)
+        {
+            Developer employee = new Developer();
+            employee.public1 = 2;
+            employee.internal1 = "4";
+            employee.protectedInternal1 = "5";
+        }
+        class Developer : Employee
+        {
+            void ITest_class()
+            {
+                Developer employee = new Developer();
+                employee.protected1 = 3;
+                employee.privateProtected1 = 6;
+            }
+        }
+    }
+}
