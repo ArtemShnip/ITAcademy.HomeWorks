c2df1128cef25bb4da469464de3ef319a7e5f267
Artem Shnip <tjoma27@gmail.com>
2020-01-19 23:46:42 +0300

    hw 10 done

---
+++ hw01/Task 1/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw_10
+{
+    class Ball
+    {
+        //https://www.codewars.com/kata/53f0f358b9cb376eca001079 
+        //Regular Ball Super Ball
+        //Create a class Ball.
+        //Ball objects should accept one argument for "ball type" when instantiated.
+        //If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+        public string ballType { get; set; }
+
+        public Ball(string ballType)
+        {
+            this.ballType = ballType;
+        }
+
+        public Ball()
+        {
+            ballType = "regular";
+        }
+    }
+}
---
+++ hw01/Task 1/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw_10
+{
+    public class Person
+    {
+        //https://www.codewars.com/kata/55a144eff5124e546400005a 
+        //Classy Classes
+        //Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
+        //Task
+        //Your task is to complete this Class, the Person class has been created.You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return
+        //johns age is 34
+
+        public string personName;
+        public int personAge;
+        public Person(string name, int age)
+        {
+            personName = name;
+            personAge = age;
+        }
+
+        public string Info
+        {
+            get { return $"{personName}s age is {personAge}"; }
+        }
+    }
+}
