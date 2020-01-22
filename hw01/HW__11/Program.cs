fb309dce5ec66461746922e6c4bc845cbc6a3c4e
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2020-01-22 17:52:37 +0300

    hw 11 done

---
+++ hw01/HW__11/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Hw_11
+{
+    class Program
+    {
+        [assembly: log4net.Config.XmlConfigurator(Watch = true)]
+        static void Main(string[] args)
+        {
+
+            string[] names = new string[] { "square", "circle", "triangle" };
+            Random rnd = new Random();
+            object[] arrayFigur = new object[5];
+
+            for (int i = 0; i < arrayFigur.Length; i++)
+            {
+                Figures figures = new Figures();
+                figures.Side = rnd.Next(1, 10);
+                figures.Type = names[rnd.Next(0, 2)];
+
+                if (figures.Type.Equals("square"))
+                {
+                    Areas square = new Areas();
+                    figures.Area = square.AreaSquare(figures.Side);
+                }
+
+                if (figures.Type.Equals("circle"))
+                {
+                    Areas circle = new Areas();
+                    figures.Area = circle.AreaCircle(figures.Side);
+                }
+
+                if (figures.Type.Equals("triangle"))
+                {
+                    Areas triangle = new Areas();
+                    figures.Area = triangle.AreaTriangle(figures.Side);
+                }
+
+                arrayFigur[i] = figures;
+            }
+
+            for (int i = 0; i < arrayFigur.Length; i++)
+            {
+                Console.WriteLine(arrayFigur[i]);
+            }
+        }
+    }
+}
