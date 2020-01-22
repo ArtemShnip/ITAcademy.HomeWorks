cdfcdb57af1ebf8515edf8301a2691bed6088dc7
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2020-01-22 17:32:44 +0300

    hw 11 done

---
+++ hw01/Hw_11/Figures.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Hw_11
+{
+    class Figures
+    {
+        public int Side { get; set; }
+
+        public string Type { get; set; }
+
+        public double Area { get; set; }
+
+        public override string ToString()
+        {
+            return $"This is {Type}. CLRType is {Type}. Area is {Area} ";
+        }
+    }
+}
